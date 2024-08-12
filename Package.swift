7320d41df7b52d1f194f3ab0b7eebd5d420e615b
Yadong Liu <yadong.github@gmail.com>
2024-08-12 16:52:53 -0700

    Updated to valid Package.Swift format

--- Package.swift
+++ Package.swift
@@ -1,4 +1,18 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-name: "ColorPicKit")
+    name: "ColorPicKit",
+    products: [
+        .library(
+            name: "ColorPicKit",
+            targets: ["ColorPicKit"]),
+    ],
+    targets: [
+        .target(
+            name: "ColorPicKit",
+            dependencies: [],
+            path: "ColorPicKit"
+        ),
+    ]
+)
